c7cc28633c66770603f901662bfe2b4bce65d512
TM <134608530+Generalsea1@users.noreply.github.com>
2026-09-09 20:18:04 +0300

    feat(household): expose current household roster boundary

--- lib/features/household/application/household_repository.dart
+++ lib/features/household/application/household_repository.dart
@@ -4,5 +4,6 @@ abstract interface class HouseholdRepository {
   Future<Household> create({required String ownerUserId, required String name});
   Future<Household?> getById(String householdId);
   Future<List<HouseholdMember>> listMemberships(String userId);
+  Future<List<HouseholdMember>> listHouseholdMembers(String householdId);
   Future<HouseholdMember> addMembership(HouseholdMember member);
 }
