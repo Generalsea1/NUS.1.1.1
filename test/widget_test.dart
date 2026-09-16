08c09e44cf39d21c7668c539f202770626a66e3c
TM <134608530+Generalsea1@users.noreply.github.com>
2026-09-16 21:36:55 +0300

    fix widget test missing Material import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:nus/features/expenses/application/expense_lifecycle_service.dart';
 import 'package:nus/features/expenses/application/expense_management_service.dart';
